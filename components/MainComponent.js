6f4a2367250d66cbb5b1d6cf382ff4fabb7dc8f0
Davtand27 <79177820+Davtand27@users.noreply.github.com>
2021-05-28 16:03:33 -0700

    Swipe & Delete Favorites

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -231,6 +231,20 @@ const MainNavigator = createDrawerNavigator(
                 )
             }
         },
+        Favorites: {
+            screen: FavoritesNavigator,
+            navigationOptions: {
+                drawerLabel: 'My Favorites',
+                drawerIcon: ({tintColor}) => (
+                    <Icon
+                        name='heart'
+                        type='font-awesome'
+                        size={24}
+                        color={tintColor}
+                    />
+                )
+            }
+        },
         About: {
             screen: AboutNavigator,
             navigationOptions: {
@@ -260,20 +274,8 @@ const MainNavigator = createDrawerNavigator(
             }
         }
     },
-    Favorites: {
-        screen: FavoritesNavigator,
-        navigationOptions: {
-            drawerLabel: 'My Favorites',
-            drawerIcon: ({tintColor}) => (
-                <Icon
-                    name='heart'
-                    type='font-awesome'
-                    size={24}
-                    color={tintColor}
-                />
-            )
-        }
-    },
+    
+    
     {
         drawerBackgroundColor: '#CEC8FF',
         contentComponent: CustomDrawerContentComponent
